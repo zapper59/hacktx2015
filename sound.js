1592f0766173137d75e06aa9d0f00f86d7f5947f
Brian Richer <bririche@cs.utexas.edu>
2015-10-27 20:54:29 -0500

    Mobile vibrate

--- sound.js
+++ sound.js
@@ -1,6 +1,8 @@
 var airhorn_sound = new Audio('./assets/airhorn.mp3')
 function play_airhorn(){
 	airhorn_sound.cloneNode().play();
+	navigator.vibrate=navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+	navigator.vibrate(600);
 }
 var song = new Audio('./assets/illuminati.mp3');
 function loop_music(){
